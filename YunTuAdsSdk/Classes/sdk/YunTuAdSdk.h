6f3f2bab47683ee647676a4afe96b856cc5c7430
hbokok <260413992@qq.com>
2025-10-16 11:28:36 +0800

    fix

--- YunTuAdsSdk/Classes/sdk/YunTuAdSdk.h
+++ YunTuAdsSdk/Classes/sdk/YunTuAdSdk.h
@@ -5,9 +5,16 @@
 //
 #import <Foundation/Foundation.h>
 
+typedef NS_ENUM (NSInteger, YunTuAcceptStatus) {
+    YunTuUnknown = 0,
+    YunTuAccepted,
+    YunTuDenied,
+};
+
 @interface YunTuAdSdk : NSObject
 +(instancetype) shareInstance;
 -(void)initWithAppId:(NSString *)appId;
 -(void)setDebugMode:(BOOL)enable;
+-(void)setGDPRStatus:(YunTuAcceptStatus)status;
 -(void) startAdPrev:(NSString *)rewardId interstitialId:(NSString *)interstitialId fullScreenId:(NSString *)fullScreenId userId:(NSString *)userId;
 @end
